b8139f15743e63fbd7946599982eee3b8f9b5d02
sagadeh <paolo.dealca@gmail.com>
2020-11-11 23:21:27 +0800

    added working files

---
+++ Working/2020/11 Nov/Spacesaver/11.03.2020/Wales and North Ireland copy/300x600-Text Only Template/studioInvocation.js
@@ -0,0 +1,86 @@
+function studioInvocation(){
+    //add studio inovcation here
+    Enabler.setProfileId(10581857);
+    var devDynamicContent = {};
+
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed = [{}];
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0]._id = 0;
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].id = 1;
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].Reporting_Label = "5f98397d694f690006bbae26-5f995515694f690006bc4189_39";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].Active = true;
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].Weights = 1;
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].Scoring_status = "Unscored";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].animationType = "frame1";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].logo = {};
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].logo.Url = "";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame1Headline = "We're still open";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame1Headline2 = "Book an eye test";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame1Headline3 = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame2Headline = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame2Headline2 = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame2Headline3 = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame3Headline = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame3Headline2 = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame3Headline3 = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].currency = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaText = "Book now";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaText2 = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaColor1 = "Racing";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaColor2 = "N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontSize1 = "30, 17, 0";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontSize2 = "0,0,0";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontSize3 = "0,0,0";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontColor1 = "Mid Green, Dark Grey, N\/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontColor2 = "N/A, N/A, N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontColor3 = "N/A, N/A, N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].customVariable = "Bold, Regular, N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].customVariable2 = "N/A, N/A, N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].customVariable3 = "N/A, N/A, N/A";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].cssAttrib = "\/* custom css *\/";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].bgColor = "White";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].bgMask1 = "Lime";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].landingPage = {};
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].landingPage.Url = "";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].startDate = {};
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].startDate.RawValue = "";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].startDate.UtcValue = 0;
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].endDate = {};
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].endDate.RawValue = "";
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].endDate.UtcValue = 0;
+    devDynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].readableLabel = "Non-targeted";
+    Enabler.setDevDynamicContent(devDynamicContent);
+
+
+    defaultValues.animationType = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].animationType;
+    defaultValues.logo = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].logo.Url;
+    defaultValues.frame1Headline = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame1Headline;
+    defaultValues.frame1Headline2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame1Headline2;
+    defaultValues.frame1Headline3 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame1Headline3;
+    defaultValues.frame2Headline = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame2Headline;
+    defaultValues.frame2Headline2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame2Headline2;
+    defaultValues.frame2Headline3 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame2Headline3;
+    defaultValues.frame3Headline = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame3Headline;
+    defaultValues.frame3Headline2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame3Headline2;
+    defaultValues.frame3Headline3 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].frame3Headline3;
+    defaultValues.currency = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].currency;
+    defaultValues.ctaText = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaText;
+    defaultValues.ctaText2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaText2;
+    defaultValues.ctaColor1 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaColor1;
+    defaultValues.ctaColor2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].ctaColor2;
+    defaultValues.fontSize1 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontSize1;
+    defaultValues.fontSize2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontSize2;
+    defaultValues.fontSize3 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontSize3;
+    defaultValues.fontColor1 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontColor1;
+    defaultValues.fontColor2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontColor2;
+    defaultValues.fontColor3 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].fontColor3;
+    defaultValues.customVariable = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].customVariable;
+    defaultValues.customVariable2 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].customVariable2;
+    defaultValues.customVariable3 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].customVariable3;
+    defaultValues.cssAttrib = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].cssAttrib;
+    defaultValues.bgColor = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].bgColor;
+    defaultValues.bgMask1 = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].bgMask1;
+    defaultValues.landingPage = dynamicContent.specs_3099422_uk_opticsao_urgentlockdown_AdLib_Feed[0].landingPage.Url;
+    
+}
+    
+   
